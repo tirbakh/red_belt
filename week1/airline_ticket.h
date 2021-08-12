19c83dda4db0e4da3a1736b22f122c7c9f5945b8
tirbakh <tirbakh1998@gmail.com>
2021-08-12 19:00:49 +0300

    Done with w1t3

--- week1/airline_ticket.h
+++ week1/airline_ticket.h
@@ -1,15 +1,43 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 struct Date {
   int year, month, day;
 };
 
+bool operator<(const Date& lhs, const Date& rhs) {
+    return vector<int>{lhs.year, lhs.month, lhs.day} < vector<int>{rhs.year, rhs.month, rhs.day};
+}
+
+bool operator==(const Date& lhs, const Date& rhs) {
+    return !(lhs < rhs || rhs < lhs);
+}
+
+ostream& operator<<(ostream& os, const Date& d) {
+    os << d.year << "-" << d.month << "-" << d.day;
+    return os;
+}
+
 struct Time {
   int hours, minutes;
 };
+
+ostream& operator<<(ostream& os, const Time& d) {
+    os << d.hours << ":" << d.minutes;
+    return os;
+}
+
+bool operator<(const Time& lhs, const Time& rhs) {
+    return vector<int>{lhs.hours, lhs.minutes} < vector<int>{rhs.hours, rhs.minutes};
+}
+
+bool operator==(const Time& lhs, const Time& rhs) {
+    return !(lhs < rhs || rhs < lhs);
+}
 
 struct AirlineTicket {
   string from;
